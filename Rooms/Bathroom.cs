ff8eeac0151d6d91a34e9e3f376d2915107dc5ab
Guillon Yvain <yvainmacguire@icloud.com>
2023-11-02 08:24:46 -0400

    Modification du jeu
    code different et toilettes ameliorer

--- Rooms/Bathroom.cs
+++ Rooms/Bathroom.cs
@@ -10,13 +10,25 @@ Tu peux revenir dans ta [chambre].
 
         internal override void ReceiveChoice(string choice)
         {
+            bool isBathTaken = false;
             switch (choice)
             {
                 case "bain":
                     Console.WriteLine("Tu te laisses relaxer dans le bain.");
+                    isBathTaken = true;
                     break;
                 case "miroir":
-                    Console.WriteLine("Tu aperçois les chiffres 2314 écrits sur la brume sur le miroir.");
+                    if (isBathTaken)
+                    {
+                        Console.WriteLine("Tu aperçois les chiffres 2314 écrits sur la brume sur le miroir.");
+                    }
+                    else if (isBathTaken == false)
+                    {
+                        Console.WriteLine("Tu fais des grimaces devant le miroir.");
+                    }
+
+
+
                     break;
                 case "chambre":
                     Console.WriteLine("Tu retournes dans ta chambre.");
