d66bf4f2ec7ba260942ef30c20d9d61b2d8eac80
Guillon Yvain <yvainmacguire@icloud.com>
2023-11-23 09:32:37 -0500

    Creation 5 minijeu
    mini jeu 1 et 2 fini
    (le jeu est bugge tant que les 5 minijeux ne sont pas fini)

---
+++ Rooms/MiniJeu3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNarratif.Rooms
+{
+    internal class MiniJeu3 : Room
+    {
+        internal override void Condition()
+        {
+            Console.Clear();
+
+
+        }
+        internal override string CreateDescription() => "Tu t'es endormis pour la troisieme fois et es actuellement en train de rever:\n(Tapez 'enter' pour continuer)";
+        internal override void ReceiveChoice(string choice)
+        {
+            string ans;
+        p1:
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Reponse: ");
+            ans = Console.ReadLine();
+            Console.Clear();
+            if (ans == "")
+            {
+
+
+            }
+            else if (ans == "")
+            {
+                Game.Transition<SecurityRoom>();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Tu te reveilles...");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else { goto p1; }
+        }
+    }
+}
---
+++ Rooms/MiniJeu5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNarratif.Rooms
+{
+    internal class MiniJeu5:Room
+    {
+
+
+        internal override void Condition()
+        {
+            Console.Clear();
+
+
+        }
+        internal override string CreateDescription() => "Tu t'es endormis pour la cinquieme fois et es actuellement en train de rever:\n(Tapez 'enter' pour continuer)";
+        internal override void ReceiveChoice(string choice)
+        {
+            string ans;
+        p1:
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Reponse: ");
+            ans = Console.ReadLine();
+            Console.Clear();
+            if (ans == "")
+            {
+
+
+            }
+            else if (ans == "")
+            {
+                Game.Transition<SecurityRoom>();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Tu te reveilles...");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else { goto p1; }
+        }
+
+
+
+    }
+}
